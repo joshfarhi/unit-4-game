0f512426b1c99502e66487aa3f771e3169746823
joshfarhi <joshfarhi@icloud.com>
2018-12-19 00:57:22 -0500

    changes made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,82 @@
-$(document).ready(function () 
-{
-    var random = Math.floor(Math.random() * 150 + 10);
-    console.log("random: " + random); 
-    $(".randomNumber").html(random); 
+$(document).ready(function () {
+    var random = Math.floor(Math.random() * 150) + 1;
+    console.log("random: " + random);
+    $(".randomNumber").html(random);
 
-    var wins = 0; 
-    console.log("wins: " + numWins); 
-    
-    var losses = 0; 
-    console.log("losses: " + numLosses); 
-    
-    var score = 0; 
-    console.log("score: " + finalScoreNumberDisplay); 
+    var wins = 0;
+    console.log("wins: " + wins);
 
-        var gemOne = Math.floor(Math.random() * 10) + 1; 
-		console.log("Gem 1: " + gemOne); 
+    var losses = 0;
+    console.log("losses: " + losses);
+
+    var score = 0;
+    console.log("score: " + score);
+
+    var gemOne = Math.floor(Math.random() * 10) + 1;
+    console.log("Gem 1: " + gemOne);
+    $("#one").html("<img src=" + "assets/images/crystal1.png" + " value=" + gemOne + ">")
+
+    var gemTwo = Math.floor(Math.random() * 10) + 1;
+    console.log("Gem 1: " + gemTwo);
+    $("#two").html("<img src=" + "assets/images/crystal2.png" + " value=" + gemTwo + ">")
+
+    var gemThree = Math.floor(Math.random() * 10) + 1;
+    console.log("Gem 1: " + gemThree);
+    $("#three").html("<img src=" + "assets/images/crystal3.png" + " value=" + gemThree + ">")
+
+    var gemFour = Math.floor(Math.random() * 10) + 1;
+    console.log("Gem 1: " + gemFour);
+    $("#four").html("<img src=" + "assets/images/crystal4.png" + " value=" + gemFour + ">")
+
+    function reset() {
+        random = Math.floor(Math.random() * 150) + 10;
+        console.log("random" + random);
+        $(".randomNumber").html(random)
+
+        score = 0;
+        $("finalScoreNum").html(score);
+
+        var gemOne = Math.floor(Math.random() * 10) + 1;
+        console.log("Gem 1: " + gemOne);
         $("#one").html("<img src=" + "assets/images/crystal1.png" + " value=" + gemOne + ">")
 
-        var gemTwo = Math.floor(Math.random() * 10) + 1; 
-		console.log("Gem 1: " + gemTwo); 
+        var gemTwo = Math.floor(Math.random() * 10) + 1;
+        console.log("Gem 1: " + gemTwo);
         $("#two").html("<img src=" + "assets/images/crystal2.png" + " value=" + gemTwo + ">")
 
-        var gemThree = Math.floor(Math.random() * 10) + 1; 
-		console.log("Gem 1: " + gemThree); 
+        var gemThree = Math.floor(Math.random() * 10) + 1;
+        console.log("Gem 1: " + gemThree);
         $("#three").html("<img src=" + "assets/images/crystal3.png" + " value=" + gemThree + ">")
 
-        var gemFour = Math.floor(Math.random() * 10) + 1; 
-		console.log("Gem 1: " + gemFour); 
+        var gemFour = Math.floor(Math.random() * 10) + 1;
+        console.log("Gem 1: " + gemFour);
         $("#four").html("<img src=" + "assets/images/crystal4.png" + " value=" + gemFour + ">")
 
-        function reset () {
-            random = Math.floor(Math.random() * 150) + 10;
-            console.log("random" + random);
-            $("")
-        }
+        $("img").on("click", function () {
+            var resetScore = score += parseInt(
+                $(this).attr("value")
+            );
 
-} 
+            console.log("New Score " + resetScore);
+            $(".scoreDisplay").html(newScore);
+
+            if (newScore === random) {
+                wins++
+                $(".wins").html("Wins " + wins)
+                console.log("Wins " + wins)
+
+                reset();
+            }
+
+            else if (newScore === randomNumber) {
+                losses++;
+                $(".losses").html("Losses " + losses)
+                console.log("Losses " + losses)
+                reset();
+
+            }
+        });
+    }
+
+
+})
